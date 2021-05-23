39d150aa72e2e33144fc31f7cb1ca828e01ceb3b
sweettime8 <nghduc91@gmail.com>
2021-05-24 00:03:08 +0700

    fix

--- src/main/java/com/elcom/video/auth/CustomUserDetails.java
+++ src/main/java/com/elcom/video/auth/CustomUserDetails.java
@@ -7,7 +7,7 @@
 //import org.springframework.security.core.GrantedAuthority;
 //import org.springframework.security.core.authority.SimpleGrantedAuthority;
 //import org.springframework.security.core.userdetails.UserDetails;
-//mrd demo13123123
+//mrd demo13123123sdassad
 ///**
 // *
 // * @author anhdv
